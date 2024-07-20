1e46ac05deb9d5093beebd5f829f8402900deb07
gyur1kim <qri1114@gmail.com>
2024-07-21 03:31:33 +0900

    :fire: 필요없는 파일 삭제

--- GlobalConfig-1721208887818.js
+++
@@ -1,5 +0,0 @@
-
-window.SuperUX = {
-    ...window.SuperUX,
-    globalConfig: {"sasServerURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","sasServerName":"gaia","uxServerURL":"","bxWebsocketURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","waplAppStoreURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","mode":"Production"}
-}
