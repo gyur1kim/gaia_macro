5d55dfe3bbf4a63be47ddb2f1ebacd00a543c42a
gyur1kim <qri1114@gmail.com>
2024-07-21 01:14:50 +0900

    :sparkles: 템플릿 파일들 복사 + 삭제 기능

--- macro.py
+++ macro.py
@@ -2,127 +2,187 @@
 import re
 import os
 import zipfile
+import shutil
 
-FILE_PATH = 'C:/Users/qri/Desktop/gaia_macro/'
-FILE_ID = '1721208887818'
+WORK_FOLDER_PATH = 'C:/Users/qri/Desktop/gaia_macro/'
+SAVE_FILE_PATH = 'C:/Users/qri/Downloads'
+FILE_ID = '1721484218170'
+
+# 선다형 파일 복사+붙여넣기
+OPTION_FILES_ORIGIN = [
+    f'{WORK_FOLDER_PATH}선다형/app-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}선다형/GlobalConfig-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}선다형/import-libraries-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}선다형/index.html',
+    f'{WORK_FOLDER_PATH}선다형/runner-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}선다형/SUIT-Bold.ttf',
+]
+
+for OPTION_FILE in OPTION_FILES_ORIGIN:
+    destination_file = os.path.join(WORK_FOLDER_PATH, os.path.basename(OPTION_FILE))
+    
+    try:
+        # 파일 복사
+        shutil.copy2(OPTION_FILE, WORK_FOLDER_PATH)
+        print(f"파일이 성공적으로 복사되었습니다: {destination_file}")
+    except FileNotFoundError:
+        print(f"원본 파일을 찾을 수 없습니다: {OPTION_FILE}")
+    except PermissionError:
+        print(f"권한이 없습니다: {destination_file}")
+    except Exception as e:
+        print(f"파일 복사 중 오류가 발생했습니다: {e}")
+
 
 # 컴포넌트명
-TEXT0_COMP_NAME = '0711_text1 19'
-TEXT1_COMP_NAME = '0711_text1 63'
-TEXT2_COMP_NAME = '0711_text1 64'
+FRANE_COMP_NAME = 'kgr_240719_frame2 65'
+
+TEXT0_COMP_NAME = '0711_text1 59'
+AUDIO_COMP_NAME = 'kgr_240716_audio3 69'
+TEXT1_COMP_NAME = '0711_text1 60'
+IMG1_COMP_NAME = 'kgr_240716_img2 63'
+BOX1_COMP_NAME = '0711_box1 5'
+EX1_COMP_NAME = '0711_ex2 9'
+TEXT2_COMP_NAME = '0711_text1 61'
+IMG2_COMP_NAME = 'kgr_240716_img2 64'
+BOX2_COMP_NAME = '0711_box1 6'
+EX2_COMP_NAME = '0711_ex2 10'
+
+OP_COMP_NAME = '0719_op 86'
 
 # 파일 경로 설정
-app_file_js = f'{FILE_PATH}app-{FILE_ID}.js'
+OP_APP_FILE = f'{WORK_FOLDER_PATH}app-{FILE_ID}.js'
 
 # 파일 읽기
-with open(app_file_js, 'r', encoding='utf-8') as file:
+with open(OP_APP_FILE, 'r', encoding='utf-8') as file:
     content = file.read()
 
 # 텍스트0 변경
-text0_value = r'텍스트0이 바뀌어야행'
+text0_value = r''
 text0_pattern = fr'("?name"?:\s?"{TEXT0_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},)'
 text0_replacement = fr'\1, "value": "{text0_value}"\2'
 
 # 오디오0 변경
-audio0_value = r''
-audio0_pattern = r'("?name"?:\s?"kgr_240716_audio3 65"[\s\S]*?content"?:\s?{[\s\S]*?filePath"?:\s?{[\s\S]*?false)[\s\S]*?(},)'
+audio0_value = ''
+audio0_pattern = fr'("?name"?:\s?"{AUDIO_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?filePath"?:\s?{{[\s\S]*?false)[\s\S]*?(}},)'
 audio0_replacement = fr'\1, "value": "{audio0_value}"\2'
 
 # 텍스트1 변경
-text1_value = r'텍스트1 <b>진해져라</b>'
+text1_value = r''
 text1_pattern = fr'("?name"?:\s?"{TEXT1_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},)'
 text1_replacement = fr'\1, "value": "{text1_value}"\2'
 
 # 이미지1 변경
 img1_filePath = ''
 img1_imgAlt = ''
-img1_pattern = r'("?name"?:\s?"kgr_240716_img2 69"[\s\S]*?content"?:\s?{[\s\S]*?filePath"?:\s?{[\s\S]*?false)[\s\S]*?(},[\s\S]*?imgAlt"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+img1_pattern = fr'("?name"?:\s?"{IMG1_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?filePath"?:\s?{{[\s\S]*?false)[\s\S]*?(}},[\s\S]*?imgAlt"?:\s?{{[\s\S]*?false)[\s\S]*?(}},?[\s\S]*?}},?)'
 img1_replacement = fr'\1, "filePath": "{img1_filePath}" \2, "imgAlt": "{img1_imgAlt}"\3'
 
 # 박스1 변경
 box1_value = r''
-box1_pattern = r'("?name"?:\s?"0711_box1 13"[\s\S]*?content"?:\s?{[\s\S]*?value"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+box1_pattern = fr'("?name"?:\s?"{BOX1_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},?[\s\S]*?}},?)'
 box1_replacement = fr'\1, "value": "{box1_value}"\2'
 
 # 보기박스1 변경
 ex1_value = r''
-ex1_pattern = r'("?name"?:\s?"0711_ex2 11"[\s\S]*?content"?:\s?{[\s\S]*?value"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+ex1_pattern = fr'("?name"?:\s?"{EX1_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},?[\s\S]*?}},?)'
 ex1_replacement = fr'\1, "value": "{ex1_value}"\2'
 
 # 텍스트2 변경
-text2_value = r'텍스트2 [!\\\\omega!]'
+text2_value = r''
 text2_pattern = fr'("?name"?:\s?"{TEXT2_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},)'
 text2_replacement = fr'\1, "value": "{text2_value}"\2'
 
 # 이미지2 변경
 img2_filePath = ''
 img2_imgAlt = ''
-img2_pattern = r'("?name"?:\s?"kgr_240716_img2 70"[\s\S]*?content"?:\s?{[\s\S]*?filePath"?:\s?{[\s\S]*?false)[\s\S]*?(},[\s\S]*?imgAlt"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+img2_pattern = fr'("?name"?:\s?"{IMG2_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?filePath"?:\s?{{[\s\S]*?false)[\s\S]*?(}},[\s\S]*?imgAlt"?:\s?{{[\s\S]*?false)[\s\S]*?(}},?[\s\S]*?}},?)'
 img2_replacement = fr'\1, "filePath": "{img2_filePath}" \2, "imgAlt": "{img2_imgAlt}"\3'
 
 # 박스2 변경
 box2_value = r''
-box2_pattern = r'("?name"?:\s?"0711_box1 14"[\s\S]*?content"?:\s?{[\s\S]*?value"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+box2_pattern = fr'("?name"?:\s?"{BOX2_COMP_NAME}"[\s\S]*?content"?:\s?{{[\s\S]*?value"?:\s?{{[\s\S]*?false)[\s\S]*?(}},?[\s\S]*?}},?)'
 box2_replacement = fr'\1, "value": "{box2_value}"\2'
 
-# 보기박스2 변경
-ex2_value = r''
-ex2_pattern = r'("?name"?:\s?"0711_ex2 12"[\s\S]*?content"?:\s?{[\s\S]*?value"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
-ex2_replacement = fr'\1, "value": "{ex2_value}"\2'
+# # 보기박스2 변경
+# ex2_value = r''
+# ex2_pattern = r'("?name"?:\s?"0711_ex2 12"[\s\S]*?content"?:\s?{[\s\S]*?value"?:\s?{[\s\S]*?false)[\s\S]*?(},?[\s\S]*?},?)'
+# ex2_replacement = fr'\1, "value": "{ex2_value}"\2'
 
-# 선지 변경
-op_row_list = r'[{"textValue": "1[!\\sqrt{300}!]", "filePath": "", "imgAlt": ""}, {"textValue": "2", "filePath": "", "imgAlt": ""}, {"textValue": "3", "filePath": "", "imgAlt": ""}, {"textValue": "4", "filePath": "", "imgAlt": ""}, {"textValue": "5", "filePath": "", "imgAlt": ""}]'
-op_answer = '1'
-op_pattern = r'("?name"?:\s?"0716_op2 66"[\s\S]*?rowList"?:\s?{[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?}[\s\S]*?correctAnswer"?:\s?{[\s\S]*?false)[\s\S]*?(},)'
-op_replacement = fr'\1{op_row_list}\2, "value": {op_answer}\3'
+# # 선지 변경
+# op_row_list = r'[{"textValue": "1[!\\sqrt{300}!]", "filePath": "", "imgAlt": ""}, {"textValue": "2", "filePath": "", "imgAlt": ""}, {"textValue": "3", "filePath": "", "imgAlt": ""}, {"textValue": "4", "filePath": "", "imgAlt": ""}, {"textValue": "5", "filePath": "", "imgAlt": ""}]'
+# op_answer = '1'
+# op_pattern = r'("?name"?:\s?"0716_op2 66"[\s\S]*?rowList"?:\s?{[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?}[\s\S]*?correctAnswer"?:\s?{[\s\S]*?false)[\s\S]*?(},)'
+# op_replacement = fr'\1{op_row_list}\2, "value": {op_answer}\3'
 
-# frame
-frame_translations = '[{"filePath": "","imgAlt": "","text": "여기는 해석이 옵니다.",},]'
-frame_explanations = '[{"filePath": "","imgAlt": "","text": "여기는 해설이 옵니다.",},]'
-frame_correct_answer = '[{"answer": "1", "filePath": "", "imgAlt": "", "text": "5"},]'
-frame_scripts = '[{ "english": "", "translation": ""}]'
-frame_pattern = r'(name"?:\s?"kgr_240716_frame3 57",[\s\S]*?translations"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?explanations"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?correctAnswer"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?scripts"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},)'
-frame_replacement = fr'\1{frame_translations}\2{frame_explanations}\3{frame_correct_answer}\4{frame_scripts}\5'
+# # frame
+# frame_translations = '[{"filePath": "","imgAlt": "","text": "여기는 해석이 옵니다.",},]'
+# frame_explanations = '[{"filePath": "","imgAlt": "","text": "여기는 해설이 옵니다.",},]'
+# frame_correct_answer = '[{"answer": "1", "filePath": "", "imgAlt": "", "text": "5"},]'
+# frame_scripts = '[{ "english": "", "translation": ""}]'
+# frame_pattern = r'(name"?:\s?"kgr_240716_frame3 57",[\s\S]*?translations"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?explanations"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?correctAnswer"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},[\s\S]*?scripts"?:[\s\S]*?value"?:\s?)\[[\s\S]*?\](,?[\s\S]*?},)'
+# frame_replacement = fr'\1{frame_translations}\2{frame_explanations}\3{frame_correct_answer}\4{frame_scripts}\5'
 
-content = re.sub(frame_pattern, frame_replacement, content, flags=re.DOTALL)
-content = re.sub(text0_pattern, text0_replacement, content, flags=re.DOTALL)
-content = re.sub(audio0_pattern, audio0_replacement, content, flags=re.DOTALL)
+# content = re.sub(frame_pattern, frame_replacement, content, flags=re.DOTALL)
+# content = re.sub(text0_pattern, text0_replacement, content, flags=re.DOTALL)
+# content = re.sub(audio0_pattern, audio0_replacement, content, flags=re.DOTALL)
 
-content = re.sub(text1_pattern, text1_replacement, content, flags=re.DOTALL)
-content = re.sub(img1_pattern, img1_replacement, content, flags=re.DOTALL)
-content = re.sub(box1_pattern, box1_replacement, content, flags=re.DOTALL)
-content = re.sub(ex1_pattern, ex1_replacement, content, flags=re.DOTALL)
+# content = re.sub(text1_pattern, text1_replacement, content, flags=re.DOTALL)
+# content = re.sub(img1_pattern, img1_replacement, content, flags=re.DOTALL)
+# content = re.sub(box1_pattern, box1_replacement, content, flags=re.DOTALL)
+# content = re.sub(ex1_pattern, ex1_replacement, content, flags=re.DOTALL)
 
-content = re.sub(text2_pattern, text2_replacement, content, flags=re.DOTALL)
-content = re.sub(img2_pattern, img2_replacement, content, flags=re.DOTALL)
-content = re.sub(box2_pattern, box2_replacement, content, flags=re.DOTALL)
-content = re.sub(ex2_pattern, ex2_replacement, content, flags=re.DOTALL)
+# content = re.sub(text2_pattern, text2_replacement, content, flags=re.DOTALL)
+# content = re.sub(img2_pattern, img2_replacement, content, flags=re.DOTALL)
+# content = re.sub(box2_pattern, box2_replacement, content, flags=re.DOTALL)
+# content = re.sub(ex2_pattern, ex2_replacement, content, flags=re.DOTALL)
 
-# 선다형
-content = re.sub(op_pattern, op_replacement, content, flags=re.DOTALL)
+# # 선다형
+# content = re.sub(op_pattern, op_replacement, content, flags=re.DOTALL)
 
-# 파일 저장
-with open(app_file_js, 'w', encoding='utf-8') as file:
-    file.write(content)
+# # 파일 저장
+# with open(app_file_js, 'w', encoding='utf-8') as file:
+#     file.write(content)
 
-print("파일이 성공적으로 업데이트되었습니다.")
+# print("파일이 성공적으로 업데이트되었습니다.")
 
-# 여러 JS 파일을 ZIP 파일로 압축
-js_files = [
-    f"{FILE_PATH}app-{FILE_ID}.js",
-    f"{FILE_PATH}GlobalConfig-{FILE_ID}.js",
-    f"{FILE_PATH}import-libraries-{FILE_ID}.js",
-    f"{FILE_PATH}7818index.html",
-    f"{FILE_PATH}runner-{FILE_ID}.js",
-    f"{FILE_PATH}SUIT-Bold.ttf"
+# # 여러 JS 파일을 ZIP 파일로 압축
+# js_files = [
+#     f"{ORIGIN_FILE_PATH}app-{FILE_ID}.js",
+#     f"{ORIGIN_FILE_PATH}GlobalConfig-{FILE_ID}.js",
+#     f"{ORIGIN_FILE_PATH}import-libraries-{FILE_ID}.js",
+#     f"{ORIGIN_FILE_PATH}7818index.html",
+#     f"{ORIGIN_FILE_PATH}runner-{FILE_ID}.js",
+#     f"{ORIGIN_FILE_PATH}SUIT-Bold.ttf"
+# ]
+
+# # 압축 파일 이름
+# zip_filename = f'{ORIGIN_FILE_PATH}7818.zip'
+
+# # zip 파일 생성 및 파일 추가
+# with zipfile.ZipFile(zip_filename, 'w') as zipf:
+#     for file in js_files:
+#         zipf.write(file, arcname=os.path.basename(file))
+
+# print(f"{zip_filename} 파일이 성공적으로 생성되었습니다.")
+
+# 선다형 파일 삭제하기
+OPTION_FILES_IN_WORK_FOLDER = [
+    f'{WORK_FOLDER_PATH}app-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}GlobalConfig-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}import-libraries-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}index.html',
+    f'{WORK_FOLDER_PATH}runner-{FILE_ID}.js',
+    f'{WORK_FOLDER_PATH}SUIT-Bold.ttf'
 ]
 
-# 압축 파일 이름
-zip_filename = f'{FILE_PATH}7818.zip'
-
-# zip 파일 생성 및 파일 추가
-with zipfile.ZipFile(zip_filename, 'w') as zipf:
-    for file in js_files:
-        zipf.write(file, arcname=os.path.basename(file))
-
-print(f"{zip_filename} 파일이 성공적으로 생성되었습니다.")
+for OPTION_FILE in OPTION_FILES_IN_WORK_FOLDER:
+    try:
+        # 파일 삭제
+        os.remove(OPTION_FILE)
+        print(f"파일이 성공적으로 삭제되었습니다: {OPTION_FILE}")
+    except FileNotFoundError:
+        print(f"파일을 찾을 수 없습니다: {OPTION_FILE}")
+    except PermissionError:
+        print(f"파일 삭제 권한이 없습니다: {OPTION_FILE}")
+    except Exception as e:
+        print(f"파일 삭제 중 오류가 발생했습니다: {e}")
