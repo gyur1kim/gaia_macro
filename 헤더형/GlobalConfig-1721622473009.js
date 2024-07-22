39419f56f4c94f4c86a13c10a5e530a6659bad08
김규리B <gyuri_kim2@tmax.co.kr>
2024-07-22 13:29:46 +0900

    :bento: 헤더형 추가

---
+++ 헤더형/GlobalConfig-1721622473009.js
@@ -0,0 +1,5 @@
+
+window.SuperUX = {
+    ...window.SuperUX,
+    globalConfig: {"sasServerURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","sasServerName":"gaia","uxServerURL":"","bxWebsocketURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","waplAppStoreURL":"wss://sag-gaia-prod-wapl.tmax-superapp.com","mode":"Production"}
+}
